81ad80de914727859776e252f5e6c66a8b91c260
jessechong <jec334@gmail.com>
2022-10-27 02:24:41 -0700

    feat: added pagination logic; refactored getPlaylist and getSongs APIs

--- src/components/playlist-details.js
+++ src/components/playlist-details.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import API from "../api-service";
 
 function PlaylistDetails(props) {
+    const getMoreSongs = () => {
+        let newPageNumber = props.songPageNumber + 1
+        console.log("getMoreSongs with page num: " + newPageNumber)
+
+        props.setSongPageNumber(newPageNumber)
+        API.getSongsWithPagination(props.playlist.id, newPageNumber)
+        .then( resp => props.addMoreSongs(resp))
+        .catch( error => console.log(error))
+    }
 
     return (
         <React.Fragment>
@@ -14,6 +26,8 @@ function PlaylistDetails(props) {
                         </div>
                       )
                     })}
+                    <br/>
+                <div className="App-button" onClick={getMoreSongs}><FontAwesomeIcon icon={faPlus}/> Fetch more songs</div>
             </div>
             ): null}
         </React.Fragment >
